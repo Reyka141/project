a402aedd31f92b9d069497ac7869e83d77ddcfc6
Andrii <youdronveduta@gmail.com>
2024-10-23 15:45:16 +0200

    update

--- app/providers/providers.js
+++ app/providers/providers.js
@@ -2,7 +2,8 @@
 
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-const manifestUrl = "https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json";
+const manifestUrl =
+  "https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json";
 
 export default function Providers({ children }) {
   return (
