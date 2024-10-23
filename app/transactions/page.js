f50d832b13eb2798001e7aa7397c6c28d71b601a
Andrii <youdronveduta@gmail.com>
2024-10-23 15:45:39 +0200

    add app/transactions/page.js

---
+++ app/transactions/page.js
@@ -0,0 +1,14 @@
+"use client";
+import { Container } from "react-bootstrap";
+import Header from "./header";
+import Body from "./body";
+
+
+export default function Home() {
+  return (
+    <Container>
+      <Header />
+      <Body/>
+    </Container>
+  );
+}
